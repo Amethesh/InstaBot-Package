e9898a0a0a91678a3a9ee75b281271f0e34fce5c
Amethesh LGP <ametheshlgp@gmail.com>
2023-02-05 23:07:12 +0530

    loop finished in caption

---
+++ caption.js
@@ -0,0 +1,70 @@
+
+let tags = [
+   "#Digital 2D",
+   "#Digital 3D",
+   "#Mixed Media",
+   "#Fan Art",
+   "#Character Design",
+   "#Character Modeling",
+   "#darthmaul",
+   "#starwars",
+   "#digitalart",
+   "#fanart",
+   "#conception",
+   "#illustration",
+   "#characterposter",
+   "#characterart",
+    ];
+
+let caption = "";
+function captionSelector(tags) {
+
+    tags.forEach(function(tag) {
+        if (tag === "#Character Modeling" || tag === "#Character Design") {
+             caption = "The tag is related to character art.";
+        } 
+
+        else if (tag === "#Anime & Manga") {
+             caption = "The string is related to anime.";
+        }
+
+        else if (tag === "#blender") {
+             caption = "The tag is related to blender.";
+        } 
+
+        else if (tag === "#Concept Art" || tag === "#concept art") {
+             caption = "The string is related to concept art.";
+        }
+
+        else if (tag === "#Fantasy") {
+             caption = "The string is related to fantasy.";
+        }
+
+        else if (tag === "#Game Art" || tag === "#game art") {
+            
+            caption = "The string is related to game.";
+            //console.log(caption)
+        }
+
+        else if (tag === "#Sketches" || tag === "#Portraits") {
+             caption = "The string is related to por.";
+        }
+
+        else if (tag === "#Vehicles") {
+             caption = "The string is related to veh.";
+        }
+        
+        else if (tag === "#Hard Surface") {
+             caption = "The string is related to hard.";
+        }
+        
+        else if (tag === "#Science Fiction") {
+             caption = "The string is related to science.";
+        }
+
+    });
+}
+
+captionSelector(tags);
+
+console.log(caption);

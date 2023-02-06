bf834020ae77e1374d580078842a470ad5c85e8d
CG Teen <43398941+Amethesh@users.noreply.github.com>
2023-02-06 12:35:24 +0530

    Removing TAB spaces

--- caption.js
+++ caption.js
@@ -1,6 +1,6 @@
 // !Use better comments extention in vscode
 // TODO To import tags from artstaion and process it through .toLowerCase 
-
+// TODO test it in instagram to see wheather the tab spaces is considered or not
 //! Use template literal `` to create a multiline stings
 let tags = [
    "#Digital 2D",
@@ -27,90 +27,95 @@ function captionSelector(tags) {
 
     tags.forEach(function(tag) {
         if (tag === "#Character Modeling" || tag === "#Character Design") {
-             caption = `${ArtHead}
-             Great character by ${Artist}
-             Follow us @cg.nation_
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             #characterdesign #artstation #artstationHQ #fanart
-             #digitalpainting #charactermodeling #3dart #lightingart
-             #artdaily #aiart
+             caption = `
+${ArtHead}
+Great character by ${Artist}
+Follow us @cg.nation_
+.
+.
+.
+.
+.
+.
+.
+.
+#characterdesign #artstation #artstationHQ #fanart
+#digitalpainting #charactermodeling #3dart #lightingart
+#artdaily #aiart
              `;
         } 
 
         else if (tag === "#Anime & Manga") {
-             caption = `${ArtHead}
-             Great anime style art by ${Artist}
-             Follow us @cg.nation_
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             #characterdesign #artstation #artstationHQ #fanart
-             #jpanart #animeedit #charactermodeling #anime #manga 
-             #aot #animeboy #animeart #animegirl #cosplay
+             caption = `
+${ArtHead}
+Great anime style art by ${Artist}
+Follow us @cg.nation_
+.
+.
+.
+.
+.
+.
+.
+.
+#characterdesign #artstation #artstationHQ #fanart
+#jpanart #animeedit #charactermodeling #anime #manga 
+#aot #animeboy #animeart #animegirl #cosplay
              `;
         }
 
         else if (tag === "#blender") {
-             caption = `${ArtHead}
-             Created in blender by ${Artist}
-             Follow us @cg.nation_
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             #blender #blender3d #blendercommunity #artstation #artstationHQ
-             #blendercycles #blenderguru #b3d #blenderart #3drender #3dartists
+             caption = `
+${ArtHead}
+Created in blender by ${Artist}
+Follow us @cg.nation_
+.
+.
+.
+.
+.
+.
+.
+.
+#blender #blender3d #blendercommunity #artstation #artstationHQ
+#blendercycles #blenderguru #b3d #blenderart #3drender #3dartists
              `;
         } 
 
         else if (tag === "#Concept Art" || tag === "#concept art") { //make sure change the statement once .toLower is implemented
-             caption = `${ArtHead}
-             Great concept art by ${Artist}
-             Follow us @cg.nation_
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             #conceptart #artstation #artstationHQ
-             #digitalpainting #charactermodeling #conceptartist #digitalart
-             #colorstudy #illustration #illustrationartist #noai
+             caption = `
+${ArtHead}
+Great concept art by ${Artist}
+Follow us @cg.nation_
+.
+.
+.
+.
+.
+.
+.
+.
+#conceptart #artstation #artstationHQ
+#digitalpainting #charactermodeling #conceptartist #digitalart
+#colorstudy #illustration #illustrationartist #noai
              `;
         }
 
         else if (tag === "#Fantasy") {
-             caption = `${ArtHead}
-             Great character by ${Artist}
-             Follow us @cg.nation_
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             .
-             #artstation #artstationHQ #digitalpainting
-             #artdaily
+             caption = `
+${ArtHead}
+Great character by ${Artist}
+Follow us @cg.nation_
+.
+.
+.
+.
+.
+.
+.
+.
+#artstation #artstationHQ #digitalpainting
+#artdaily
              `;
         }
 
